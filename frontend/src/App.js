1bba18d02240cfc1e836fc2ddfda3c443a201a54
alessandra <alessandraamontanher@gmail.com>
2019-10-30 16:14:26 -0300

    starting first page

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <div className="content">
+          <form>
+            <label>ID:</label>
+            <input type="text" name="name"/>
+          </form>
+        </div>
+    </div>
+  );
+}
+
+export default App;
