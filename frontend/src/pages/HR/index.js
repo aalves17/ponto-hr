4afc801b339a70779f4d28f257c5006b25595e12
alessandra <alessandraamontanher@gmail.com>
2019-11-06 14:36:23 -0300

    adding icons in dashboard js

--- frontend/src/pages/HR/index.js
+++ frontend/src/pages/HR/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import '../HR/HR.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUserClock, faUser, faList, faDatabase, faCheck} from '@fortawesome/free-solid-svg-icons'
 
 class HR extends React.Component{
     render(){
@@ -10,11 +12,11 @@ class HR extends React.Component{
                         <h1 align="center">img</h1>
                     </div>
                     <ul>
-                        <li><a href="#">Register Ponto</a></li>
-                        <li><a href="#">Register New Employee</a></li>
-                        <li><a href="#">Change Employee Data</a></li>
-                        <li><a href="#">Espelho</a></li>
-                        <li><a href="#">Check Marcações</a></li>
+                        <li><a href="/hr"><FontAwesomeIcon icon={faUserClock} /> Register Ponto</a></li>
+                        <li><a href="/hr"><FontAwesomeIcon icon={faUser}/> Register New Employee</a></li>
+                        <li><a href="/hr"><FontAwesomeIcon icon={faDatabase}/> Change Employee Data</a></li>
+                        <li><a href="/hr"><FontAwesomeIcon icon={faList}/> Espelho</a></li>
+                        <li><a href="/hr"><FontAwesomeIcon icon={faCheck}/> Check Marcações</a></li>
                     </ul>
                 </div>
 
