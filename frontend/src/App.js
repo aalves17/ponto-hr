dfe7a82a53422b5382992bff50e1c9941d339e29
alessandra <alessandraamontanher@gmail.com>
2019-11-06 11:06:50 -0300

    building css for hr dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import logo from './assets/logo.png'
 
 import Routes from './routes'
 
--- frontend/src/pages/HR/index.js
+++ frontend/src/pages/HR/index.js
@@ -1,8 +1,32 @@
 import React from 'react';
+import '../HR/HR.css';
 
 class HR extends React.Component{
     render(){
-        return <h1>dashboard HR</h1>
+        return (
+            <>
+                <div className="sidebar">
+                    <div className="logo">
+                        <h1 align="center">img</h1>
+                    </div>
+                    <ul>
+                        <li><a href="#">Register Ponto</a></li>
+                        <li><a href="#">Register New Employee</a></li>
+                        <li><a href="#">Change Employee Data</a></li>
+                        <li><a href="#">Espelho</a></li>
+                        <li><a href="#">Check Marcações</a></li>
+                    </ul>
+                </div>
+
+                <div className="notification-bar">
+
+                </div>
+
+                <div className="app-main-container">
+                    <p>app-main-container</p>
+                </div>
+            </>
+        )
     }
 }
 
