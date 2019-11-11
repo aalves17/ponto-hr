4ddd082ea8253510a71fce7951b7354c86737441
alessandra <alessandraamontanher@gmail.com>
2019-11-11 16:12:29 -0300

    trying SPA

---
+++ frontend/src/pages/HR/NewEmp.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class NewEmp extends React.Component {
+  render() {
+    return (
+      <div className="app-main-container">
+        <h1>render form()</h1>
+      </div>
+    );
+  }
+}
+
+export default NewEmp;
--- frontend/src/pages/HR/index.js
+++ frontend/src/pages/HR/index.js
@@ -1,19 +1,23 @@
 import React from 'react';
+import { NavLink, Route, HashRouter } from 'react-router-dom';
 import '../HR/HR.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUserClock, faUser, faList, faDatabase, faCheck} from '@fortawesome/free-solid-svg-icons'
+
+import NewEmp from "./NewEmp";
 
 class HR extends React.Component{
     render(){
         return (
             <>
+            <HashRouter>
                 <div className="sidebar">
                     <div className="logo">
                         <h1 align="center">img</h1>
                     </div>
                     <ul>
                         <li><a href="/hr"><FontAwesomeIcon icon={faUserClock} /> Register Ponto</a></li>
-                        <li><a href="/hr"><FontAwesomeIcon icon={faUser}/> Register New Employee</a></li>
+                        <li><NavLink  to="/NewEmp"><FontAwesomeIcon icon={faUser}/> Register New Employee</NavLink ></li>
                         <li><a href="/hr"><FontAwesomeIcon icon={faDatabase}/> Change Employee Data</a></li>
                         <li><a href="/hr"><FontAwesomeIcon icon={faList}/> Espelho</a></li>
                         <li><a href="/hr"><FontAwesomeIcon icon={faCheck}/> Check Marcações</a></li>
@@ -24,9 +28,10 @@ class HR extends React.Component{
 
                 </div>
 
-                <div className="app-main-container">
-                    <p>app-main-container</p>
+                <div className="rotas">
+                    <Route exact path="/NewEmp" component={NewEmp}/>
                 </div>
+            </HashRouter>
             </>
         )
     }
